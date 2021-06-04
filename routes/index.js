2bca1570a934bdc29774f347567eb78ea5fa6300
AWFinalOnk <awfinalonk@gmail.com>
2021-06-04 10:42:49 +0000

    Login donete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+const publicDirectory = path.join(__dirname, '../public');
+const rootDirectory = path.join(__dirname,'../')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('login.html', {root: publicDirectory});
 });
 
 module.exports = router;
